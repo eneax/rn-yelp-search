0af6a2bec2dcec3fa024e7a916e0b3c73fc10153
eneax <19818937+eneax@users.noreply.github.com>
2021-08-21 18:23:26 +0200

    Managing TextInput State

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,10 +4,13 @@ import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = React.useState("");
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
