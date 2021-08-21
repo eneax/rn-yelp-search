5017d89f4b53ed1d737244e6eca0d8cf93584f14
eneax <19818937+eneax@users.noreply.github.com>
2021-08-21 18:32:25 +0200

    onEndEditing

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,15 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = ({ term, onTermChange }) => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
       <TextInput
         placeholder="Search"
         value={term}
-        onChangeText={(newTerm) => onTermChange(newTerm)}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
         autoCapitalize="none"
         autoCorrect={false}
         style={styles.inputStyle}
