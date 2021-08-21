9ec712100d97c8d54edad3a40be9417a8746368e
eneax <19818937+eneax@users.noreply.github.com>
2021-08-21 18:06:27 +0200

    Style SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View style={styles.background}>
-      <Feather name="search" size={30} color="black" />
-      <Text>Search bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput placeholder="Search" style={styles.inputStyle} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
+    marginVertical: 10,
+    flexDirection: "row",
+  },
+  iconStyle: {
+    fontSize: 30,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
   },
 });
 
