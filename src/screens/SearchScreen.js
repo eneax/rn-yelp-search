1b68fce06729eae03ace58ddcb78b88f41edf8a0
eneax <19818937+eneax@users.noreply.github.com>
2021-08-22 17:28:46 +0200

    Error Handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,22 +8,27 @@ import SearchBar from "../components/SearchBar";
 const SearchScreen = () => {
   const [term, setTerm] = React.useState("");
   const [results, setResults] = React.useState([]);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term: term,
-        location: "berlin",
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: term,
+          location: "berlin",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong");
+    }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
