2986286d1abad736ac8e99863e4e5f27adf14382
eneax <19818937+eneax@users.noreply.github.com>
2021-08-22 18:06:10 +0200

    The useEffect Hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -10,12 +10,12 @@ const SearchScreen = () => {
   const [results, setResults] = React.useState([]);
   const [errorMessage, setErrorMessage] = React.useState("");
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term: term,
+          term: searchTerm,
           location: "berlin",
         },
       });
@@ -25,9 +25,18 @@ const SearchScreen = () => {
     }
   };
 
+  // Call searchApi when component is first rendered
+  React.useEffect(() => {
+    searchApi("pizza");
+  }, []);
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
