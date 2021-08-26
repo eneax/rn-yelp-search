df333e01cad0830c3e1aadc7edeec3df675a21fc
eneax <19818937+eneax@users.noreply.github.com>
2021-08-26 21:24:04 +0200

    Add more info ResultDetail

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 
 const ResultsDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image
+        source={{
+          uri: result.image_url,
+        }}
+        style={styles.imageStyles}
+      />
+      <Text style={styles.nameStyles}>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} reviews
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyles: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  nameStyles: {
+    fontWeight: "bold",
+  },
+});
 
 export default ResultsDetail;
