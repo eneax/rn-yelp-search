ba74f18ab579dfa0e640d9e598c86eb95963a14e
eneax <19818937+eneax@users.noreply.github.com>
2021-08-27 20:47:54 +0200

    Fetching a Single Restaurant

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-function DetailsScreen({ route }) {
+import yelp from "../api/yelp";
+
+const DetailsScreen = ({ route }) => {
+  const [business, setBusiness] = React.useState(null);
   const { id } = route.params;
+
+  // fetch single business
+  const getBusinessDetail = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setBusiness(response.data);
+  };
+
+  React.useEffect(() => {
+    getBusinessDetail(id);
+  }, []);
+
+  console.log(business);
 
   return (
     <View>
-      <Text>Details Screen</Text>
+      <Text>business</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({});
 
